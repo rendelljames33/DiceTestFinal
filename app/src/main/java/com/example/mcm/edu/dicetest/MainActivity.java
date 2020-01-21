eb005bd76fe20ef7863d56e542b5b29cd5d79382
rendogs <rendelljames20001@gmail.com>
2020-01-21 22:38:05 +0800

    fourth commit

--- app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
+++ app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
+
+
 public class MainActivity extends AppCompatActivity {
     public TextView number;
     public Random rng = new Random(6);
