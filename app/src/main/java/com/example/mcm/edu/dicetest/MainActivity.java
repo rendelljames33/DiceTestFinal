39c1f3f71278f3f714190dc6395145b7b9343b93
rendogs <rendelljames20001@gmail.com>
2020-01-21 21:19:13 +0800

    fourth commit

--- app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
+++ app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.Random;
 
+
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
