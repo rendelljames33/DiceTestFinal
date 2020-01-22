28815e980cc6ab7ad4bc64619e726319d0559335
rendogs <rendelljames20001@gmail.com>
2020-01-22 21:48:33 +0800

    fourth commit

--- app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
+++ app/src/main/java/com/example/mcm/edu/dicetest/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import java.util.Random;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
 
